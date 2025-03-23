2f1c9af107223df10d69ec67cdc143502eed9f03
xa-team <57245851+xa-team@users.noreply.github.com>
2025-03-23 17:06:30 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 172 ms, Memory: 23980 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
@@ -2,14 +2,14 @@ using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()))
 using var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
 int n = int.Parse(sr.ReadLine());
-List<(int x, int y)> coordnatesList = new List<(int x, int y)>();
+List<(int x, int y)> c = new List<(int x, int y)>();
 
 for(int i = 0; i < n; i++){
-    int[] tmpCoordnates = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
-    coordnatesList.Add((tmpCoordnates[0], tmpCoordnates[1]));
+    int[] t = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    c.Add((t[0], t[1]));
 }
 
-var sortedList = coordnatesList.OrderBy(x => x).ThenBy(y => y).ToList();
+var s = c.OrderBy(x => x).ThenBy(y => y).ToList();
 
-foreach(var (x, y) in sortedList)
-    sw.WriteLine($"{x} {y}");
+foreach(var (x, y) in s)
+    sw.Write($"{x} {y}\n");
