68bfea4a9347f040b7efc2ca5e62b549d0f34802
xa-team <57245851+xa-team@users.noreply.github.com>
2025-03-23 20:03:22 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 52 ms, Memory: 5544 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cs
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cs
@@ -6,7 +6,6 @@ int minCnt = -1, c = 0;
 for (int i = 0; i < cbInfo.n; i++)
     chessboard[i] = Console.ReadLine();
 
-
 for (int y = 0; y < cbInfo.n - 7; y++)
 {
     for (int x = 0; x < cbInfo.m - 7; x++)
@@ -18,7 +17,6 @@ for (int y = 0; y < cbInfo.n - 7; y++)
             minCnt = tmpCnt;
     }
 }
-// Console.Write($"minCnt: {minCnt}, C : {c}");
 Console.Write(minCnt);
 
 int getCnt(string[] cb, int min, int x, int y)
@@ -34,24 +32,20 @@ int getCnt(string[] cb, int min, int x, int y)
             if((i - y)%2==0){ // 실제로는 홀수번째 행
                 if((j - x)%2==0){ // 실제로는 홀수번째 열
                     if(cb[i][j] != fc_W){
-                        // Console.WriteLine($"cb: {cb[i][j]} fcW: {fc_W}");
                         tmp_W++;
                     }
                         
                     if(cb[i][j] != fc_B){
-                        // Console.WriteLine($"cb: {cb[i][j]} fcB: {fc_B}");
                         tmp_B++;
                     }
                         
                 }
                 else{ // 실제로는 짝수번째 열
                     if(cb[i][j] == fc_W){
-                        // Console.WriteLine($"cb: {cb[i][j]} fc: {fc_W}");
                         tmp_W++;
                     }
                         
                     if(cb[i][j] == fc_B){
-                        // Console.WriteLine($"cb: {cb[i][j]} fcB: {fc_B}");
                         tmp_B++;
                     }
                         
@@ -62,7 +56,6 @@ int getCnt(string[] cb, int min, int x, int y)
                     if(cb[i][j] == fc_W)
                         tmp_W++;
                     if(cb[i][j] == fc_B){
-                        // Console.WriteLine($"cb: {cb[i][j]} fcB: {fc_B}");
                         tmp_B++;
                     }
                         
@@ -84,7 +77,6 @@ int getCnt(string[] cb, int min, int x, int y)
         }
     }
     if (check){
-        // Console.WriteLine($"b : {tmp_B} w: {tmp_W}");
         if(tmp_B < tmp_W)
             return tmp_B;
         else if(tmp_W < tmp_B)
@@ -94,7 +86,6 @@ int getCnt(string[] cb, int min, int x, int y)
     }
         
     else{
-        // Console.WriteLine("return -1");
         return -1;
     }
         
