4daee308442d6e049a5d0e69e09d00bb7d35ba20
xa-team <57245851+xa-team@users.noreply.github.com>
2025-03-27 15:56:52 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cs
+++ 백준/Silver/9012. 괄호/괄호.cs
@@ -1,14 +1,10 @@
-int n = int.Parse(Console.ReadLine()), openCnt, closedCnt;
+int n = int.Parse(Console.ReadLine()), openCnt;
 for(int i = 0;i<n;i++){
     openCnt = 0;
-    closedCnt = 0;
     var str = Console.ReadLine();
-    for(int j=0;j<str.Length;j++){
-        if(str[j] == '(') openCnt++;
-        else if(str[j] == ')'){
-            if(--openCnt < 0)
-                break;
-        }
-    }
+    foreach(var c in str)
+        if(c == '(') openCnt++;
+        else if(c == ')' && --openCnt < 0)
+            break;
     Console.WriteLine(openCnt==0?"YES":"NO");
 }
