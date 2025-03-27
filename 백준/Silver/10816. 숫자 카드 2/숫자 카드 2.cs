0dff96407d6d59553d7b2d1ddb8addb1b95e2392
xa-team <57245851+xa-team@users.noreply.github.com>
2025-03-27 17:09:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 416 ms, Memory: 96080 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
@@ -5,8 +5,10 @@ using var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())
 StringBuilder sb = new StringBuilder();
 
 Dictionary<int, int> hashMap = new Dictionary<int, int>();
-sr.ReadLine();
+sr.ReadLine(); // n 입력 받는데 n 필요없음.
 int[] str1 = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+// 숫자들을 입력 받고 hashMap에 있는지 체크 후 없으면 1, 있으면 +1
 foreach(int i in str1){
     if(hashMap.TryGetValue(i, out int cnt))
         hashMap[i] = cnt+1;
@@ -16,6 +18,8 @@ foreach(int i in str1){
 
 sr.ReadLine();
 int[] str2 = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+// 숫자들이 있는지 체크 후 있으면 출력하고, 없으면 0 출력
 foreach(int i in str2)
     if(hashMap.TryGetValue(i, out int value))
         sb.Append($"{value} ");
