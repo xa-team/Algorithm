566aa1cd86392d2de1dda7d5775d873f1b1a617d
xa-team <57245851+xa-team@users.noreply.github.com>
2025-04-04 15:04:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cs
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cs
@@ -1,5 +1,6 @@
 int n = int.Parse(Console.ReadLine());
 int cnt = 5000;
+int tmp = n;
 
 if (n % 3 == 0)
 {
@@ -10,14 +11,14 @@ if (n % 5 == 0)
 {
     cnt = Math.Min(cnt, n / 5);
 }
-int tmp = n;
-for (int i = 0; tmp > 0; i++, tmp -= 3)
+
+for (int i = 0; tmp > 0; i++, tmp -= 5)
 {
-    if (tmp % 5 == 0)
+    if (tmp % 3 == 0)
     {
         int tmpCnt = 0;
         tmpCnt += i;
-        tmpCnt += tmp / 5;
+        tmpCnt += tmp / 3;
         cnt = Math.Min(cnt, tmpCnt);
     }
 }
