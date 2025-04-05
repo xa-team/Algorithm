e2b08fd2a97d2e05a3be5cd397a78633d44f7e34
xa-team <57245851+xa-team@users.noreply.github.com>
2025-04-05 17:51:10 +0900

    [Silver III] Title: 소수 구하기, Time: 108 ms, Memory: 10820 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cs
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cs
@@ -1,31 +1,28 @@
-int[] numArr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+using System.Text;
 
+int[] numArr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int m = numArr[0];
 int n = numArr[1];
 
 bool[] isPrimeArr = new bool[n+1];
 
-for(int i = 2; i <= n; i++)
-{
-    isPrimeArr[i] = true;
-}
+for(int i = 2; i <= n; i++) isPrimeArr[i] = true;
 
-for(int i = 2; i <= n; i++)
+for(int i = 2; i * i <= n; i++)
 {
-    if(isPrimeArr[i] == false)
-    {
-        continue;
-    }
+    if(!isPrimeArr[i]) continue;
 
-    for(int j = i * 2; j <= n; j += i)
+    for(int j = i * i; j <= n; j += i)
     {
         isPrimeArr[j] = false;
     }
 }
+
+var sb = new StringBuilder();
+
 for(int i = m; i <= n; i++)
 {
-    if(isPrimeArr[i])
-    {
-        Console.WriteLine(i);
-    }
+    if(isPrimeArr[i]) sb.AppendLine(i.ToString());
 }
+
+Console.Write(sb);
