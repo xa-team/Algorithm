b2e3918b9dcee360cd3093dd294ea4e5925d719d
xa-team <57245851+xa-team@users.noreply.github.com>
2025-04-07 02:06:47 +0900

    [Bronze II] Title: Hashing, Time: 52 ms, Memory: 5760 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cs
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -1,12 +1,19 @@
-int length = int.Parse(Console.ReadLine());
-int hash = 0;
+using System.Numerics;
 
+int length = int.Parse(Console.ReadLine());
+BigInteger hash = 0;
+BigInteger R = 1;
+int MOD = 1234567891;
 string str = Console.ReadLine();
 
 for(int i = 0; i < length; i++)
 {
     int num = str[i] - 96;
-    hash += num * (int)Math.Pow(31, i);
+    hash += num * R;
+    R *= 31;
+
+    hash %= MOD;
+    R %= MOD;
 }
 
 Console.Write(hash);
