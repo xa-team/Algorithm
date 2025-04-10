4bffd7e99c9ddf5202c1f8aa3112fdb57519309b
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-10 16:05:31 +0900

    [Silver II] Title: 랜선 자르기, Time: 56 ms, Memory: 6168 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cs
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cs
@@ -1,13 +1,22 @@
+//6236KB, 56ms, 658B
+
 using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
 
 long[] input = Array.ConvertAll(sr.ReadLine().Split(), long.Parse);
 int K = (int)input[0];
 long N = input[1];
 
-long[] cables = new long[K];
-for (int i = 0; i < K; i++) cables[i] = long.Parse(sr.ReadLine());
+long[] cableArr = new long[K]; // 리스트보다 배열이 빠르고 메모리 덜 먹음.
+for (int i = 0; i < K; i++) cableArr[i] = long.Parse(sr.ReadLine());
 
-long right = cables.Max();
+Span<long> cables = cableArr; // Span으로 감쌈.
+
+long right = cables[0];
+for (int i = 1; i < cables.Length; i++) // Max 구하는거 Span으로 감쌌기에 lINQ보다 배열 순회가 빠름.
+{
+    if (cables[i] > right) right = cables[i];
+}
+
 long left = 1;
 long result = 0;
 
@@ -16,7 +25,7 @@ while (left <= right)
     long mid = (left + right) / 2;
     long count = 0;
 
-    foreach (long cable in cables)
+    foreach (long cable in cableArr)
     {
         count += cable / mid;
     }
