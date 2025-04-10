b52a49b2f503610e7b42660540c86591562791f3
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-10 12:23:45 +0900

    [Silver II] Title: 통계학, Time: 184 ms, Memory: 23760 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cs
+++ 백준/Silver/2108. 통계학/통계학.cs
@@ -1,18 +1,21 @@
-int N = int.Parse(Console.ReadLine());
+using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+using var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+int N = int.Parse(sr.ReadLine());
 
 Dictionary<int, int> numDic = new Dictionary<int, int>();
 List<int> list = new List<int>();
 for (int i = 0; i < N; i++)
 {
-    int num = int.Parse(Console.ReadLine());
+    int num = int.Parse(sr.ReadLine());
     list.Add(num);
     numDic[num] = numDic.GetValueOrDefault(num, 0) + 1;
 }
 
-Console.WriteLine((int)Math.Round((double)list.Sum() / N)); // 산술 평균 출력
+sw.WriteLine((int)Math.Round((double)list.Sum() / N)); // 산술 평균 출력
 
 list.Sort();
-Console.WriteLine(list[N / 2]); // 중앙값 출력
+sw.WriteLine(list[N / 2]); // 중앙값 출력
 
 int countMax = numDic.Values.Max();
 var sortedCountList = numDic
@@ -22,8 +25,6 @@ var sortedCountList = numDic
     .Select(x => x.Key)
     .ToList();
 
-Console.WriteLine(sortedCountList.Count > 1 ? sortedCountList[1] : sortedCountList[0]);
+sw.WriteLine(sortedCountList.Count > 1 ? sortedCountList[1] : sortedCountList[0]);// 최빈값 출력
 
-// 최빈값 출력
-
-Console.WriteLine(numDic.Keys.Max() - numDic.Keys.Min()); // 범위 출력
+sw.WriteLine(numDic.Keys.Max() - numDic.Keys.Min()); // 범위 출력
