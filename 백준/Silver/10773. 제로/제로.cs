2b4eb8f291f50c706ca66c1a66c4a46b6da5b242
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-14 14:54:21 +0900

    [Silver IV] Title: 제로, Time: 60 ms, Memory: 9280 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cs
+++ 백준/Silver/10773. 제로/제로.cs
@@ -3,18 +3,19 @@ using var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()))
 int k = int.Parse(sr.ReadLine());
 
 Stack<int> st = new();
-
+int sum = 0;
 while (k-- > 0)
 {
     int n = int.Parse(sr.ReadLine());
 
     if (n == 0)
     {
-        st.Pop();
+        sum -= st.Pop();
     }
     else
     {
         st.Push(n);
+        sum += n;
     }
 }
-Console.Write(st.Sum());
+Console.Write(sum);
