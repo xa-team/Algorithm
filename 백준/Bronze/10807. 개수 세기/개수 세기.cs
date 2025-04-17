94742e0a43a7065a61e2b551a60266fb059f2110
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-17 22:31:22 +0900

    [Bronze V] Title: 개수 세기, Time: 56 ms, Memory: 5748 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cs
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cs
@@ -1,9 +1,9 @@
 int n = int.Parse(Console.ReadLine());
-int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int[] count = new int[202];
+IEnumerable<int> input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] count = new int[201];
 Array.Fill(count, 0);
 foreach (int i in input)
 {
-    count[i + 101]++;
+    count[i + 100]++;
 }
-Console.Write(count[int.Parse(Console.ReadLine()) + 101]);
+Console.Write(count[int.Parse(Console.ReadLine()) + 100]);
