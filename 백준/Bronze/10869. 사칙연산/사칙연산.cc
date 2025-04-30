9c745e17f6fa2604da899fb1a0b74df0510d94db
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-30 20:08:42 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+// 10869 사칙연산
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, n1, n2;
+    cin >> a >> b;
+
+    cout << a + b << '\n'
+         << a - b << '\n'
+         << a * b << '\n'
+         << a / b << '\n'
+         << a % b;
+}
