14ab873fe54d2d8bf24ea8f314abb1f1a7d13435
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-30 18:07:19 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,14 @@
+// 1000. A+B
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+
+    cin >> a >> b;
+    cout << a + b << endl;
+}
