cce26e5c22bc156d2cb790f3e7bd5248b8c71709
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-30 19:27:35 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,24 @@
+// 1330. 두 수 비교하기
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b)
+    {
+        cout << '>';
+    }
+    else if (a < b)
+    {
+        cout << '<';
+    }
+    else
+    {
+        cout << "==";
+    }
+}
