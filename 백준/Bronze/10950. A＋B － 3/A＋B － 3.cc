3b587f3084a791239797f6ae96bd13718bb4c6ff
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-30 20:13:55 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+// 10950. A + B - 3
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
