8b3dfff7284a0bdce3bc23fab523e4b2026ae8df
Xateam <57245851+xa-team@users.noreply.github.com>
2025-04-30 16:22:25 +0900

    [Gold V] Title: 토마토, Time: 184 ms, Memory: 31056 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cs
+++ 백준/Gold/7576. 토마토/토마토.cs
@@ -9,7 +9,10 @@ int N = input[1];
 //그래프 만드는 함수
 int[,] graph = new int[N, M];
 List<(int y, int x)> startPoints = new List<(int y, int x)>();
-int wallCount = 0;
+Queue<(int x, int y)> queue = new();
+
+int unripeCount = 0; // 익어야할 토마토 개개수
+int ripeCount = 0; // 익은 토마토 개수(BFS로 익힌 것만만)
 
 for (int y = 0; y < N; y++)
 {
@@ -20,11 +23,11 @@ for (int y = 0; y < N; y++)
         graph[y, x] = arr[x];
         if (arr[x] == 1)
         {
-            startPoints.Add((y, x));
+            queue.Enqueue((x, y)); //초기 상태에서 익어있는 토마토
         }
-        else if (arr[x] == -1)
+        else if (arr[x] == 0)
         {
-            wallCount++;
+            unripeCount++;
         }
     }
 }
@@ -33,51 +36,30 @@ for (int y = 0; y < N; y++)
 int[] dx = { -1, 1, 0, 0 };
 int[] dy = { 0, 0, -1, 1 };
 
-int dayCount = 0;
-int queueSize = startPoints.Count;
-
-Queue<(int x, int y, int day)> queue = new();
-foreach (var coords in startPoints)
-{
-    queue.Enqueue((coords.x, coords.y, 0));
-}
+int day = -1;
 
 while (queue.Count > 0)
 {
-    var (x, y, day) = queue.Dequeue();
+    int queueSize = queue.Count;
+    day++; // 하루 경과
 
-    for (int direction = 0; direction < 4; direction++)
+    for (int i = 0; i < queueSize; i++)
     {
-        int nextX = x + dx[direction];
-        int nextY = y + dy[direction];
+        var (x, y) = queue.Dequeue();
+        for (int direction = 0; direction < 4; direction++)
+        {
+            int nextX = x + dx[direction];
+            int nextY = y + dy[direction];
 
-        if (nextX < 0 || nextY < 0 || nextX >= M || nextY >= N)
-        {
-            continue;
-        }
-        if (graph[nextY, nextX] == 0)
-        {
+            if (nextX < 0 || nextY < 0 || nextX >= M || nextY >= N) continue;
+            if (graph[nextY, nextX] != 0) continue;
+
             graph[nextY, nextX] = 1;
-            queue.Enqueue((nextX, nextY, day + 1));
+            queue.Enqueue((nextX, nextY));
+            ripeCount++;
         }
-        else
-        {
-            continue;
-        }
-    }
-    if (day > dayCount)
-    {
-        dayCount++;
     }
 }
 
-int sum = 0;
-for (int i = 0; i < N; i++)
-{
-    for (int j = 0; j < M; j++)
-    {
-        sum += graph[i, j];
-    }
-}
-
-Console.Write(sum == N * M - (wallCount * 2) ? dayCount : -1);
+// 안 익어있던 토마토가 전부 익었는지 확인
+Console.Write(ripeCount == unripeCount ? day : -1);
