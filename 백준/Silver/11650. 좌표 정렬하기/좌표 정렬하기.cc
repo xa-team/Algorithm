0f9c352f06d41a3e4bad43414a68651b13765076
Xateam <57245851+xa-team@users.noreply.github.com>
2025-05-01 21:26:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 52 ms, Memory: 7536 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,38 @@
+// 11650. 좌표 정렬하기
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+void swapVector(vector<vector<int>> &coords, int i)
+{
+    vector<int> tmp;
+    tmp = coords[i];
+    coords[i] = coords[i + 1];
+    coords[i + 1] = tmp;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vector<vector<int>> coords(N, vector<int>(2));
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> coords[i][0] >> coords[i][1];
+    }
+    sort(coords.begin(), coords.end(), [](const vector<int> &a, const vector<int> &b)
+         {
+             if (a[0] == b[0])
+                 return a[1] < b[1]; // x가 같으면 y 오름차순
+             return a[0] < b[0];     // x 오름차순
+         });
+    for (int i = 0; i < N; i++)
+    {
+        cout << coords[i][0] << ' ' << coords[i][1] << '\n';
+    }
+}
