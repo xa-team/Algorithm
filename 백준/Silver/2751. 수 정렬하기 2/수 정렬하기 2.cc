2c68db61c0c85f6c24895e5f9ebe97524b0ae697
Xateam <57245851+xa-team@users.noreply.github.com>
2025-05-07 13:19:31 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 284 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,21 +1,59 @@
-// 2751 수 정렬하기 2
+// 2751. 수 정렬하기 2
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
+const int MAX = 1000001;
+int arr[MAX];
+int tmp[MAX];
+
+void MergeSort(int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int mid = (left + right) >> 1; // >> 1 은 / 2 랑 같은 연산
+
+    MergeSort(left, mid);
+    MergeSort(mid + 1, right);
+
+    int p1 = left, p2 = mid + 1, p3 = left;
+    while (p1 <= mid && p2 <= right)
+    {
+        if (arr[p1] < arr[p2])
+            tmp[p3++] = arr[p1++];
+        else
+            tmp[p3++] = arr[p2++];
+    }
+
+    while (p1 <= mid)
+        tmp[p3++] = arr[p1++];
+
+    while (p2 <= right)
+        tmp[p3++] = arr[p2++];
+
+    for (int i = left; i <= right; i++)
+    {
+        arr[i] = tmp[i];
+    }
+}
+
 int main()
 {
-    int N;
-    cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    vector<int> v(N);
-    for (auto &num : v)
-        cin >> num;
+    int n;
+    cin >> n;
 
-    sort(v.begin(), v.end());
-    for (const auto &num : v)
-        cout << num << '\n';
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    MergeSort(0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << '\n';
+    }
 }
