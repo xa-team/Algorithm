f8345a5733d336727d1028a98e3a9b0e8462fe36
Xateam <57245851+xa-team@users.noreply.github.com>
2025-05-08 15:32:30 +0900

    [Silver IV] Title: 큐, Time: 212 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,65 @@
+// 10845. 큐
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+
+    int n;
+    cin >> n;
+
+    while (n-- > 0)
+    {
+        string cmd;
+        cin >> cmd;
+
+        if (cmd == "push")
+        {
+            int i;
+            cin >> i;
+            q.push(i);
+        }
+        else if (cmd == "pop")
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                int front = q.front();
+                cout << front << '\n';
+                q.pop();
+            }
+        }
+        else if (cmd == "size")
+        {
+            cout << q.size() << '\n';
+        }
+        else if (cmd == "empty")
+        {
+            if (q.empty())
+                cout << 1 << '\n';
+            else
+                cout << 0 << '\n';
+        }
+        else if (cmd == "front")
+        {
+            if (q.empty())
+                cout << -1 << '\n';
+            else
+                cout << q.front() << '\n';
+        }
+        else if (cmd == "back")
+        {
+            if (q.empty())
+                cout << -1 << '\n';
+            else
+                cout << q.back() << '\n';
+        }
+    }
+}
