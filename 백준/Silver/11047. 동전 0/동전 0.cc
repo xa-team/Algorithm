315c8f184f0c64011e5619ba33018167aeeef05c
Xateam <57245851+xa-team@users.noreply.github.com>
2025-05-19 14:46:42 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,32 @@
+// 11047. 동전 0
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    int coins[N];
+    for (int i = 0; i < N; ++i)
+        cin >> coins[i];
+
+    int total = K;
+    int index = N - 1;
+    int count = 0;
+    while (total > 0)
+    {
+        if (total - coins[index] >= 0)
+        {
+            total -= coins[index];
+            ++count;
+        }
+        else
+        {
+            --index;
+        }
+    }
+    cout << count;
+}
