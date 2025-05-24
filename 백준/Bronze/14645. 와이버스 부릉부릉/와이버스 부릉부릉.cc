b8b3e5b5db27893eaf7ae27adc871d024667cf02
Xateam <57245851+xa-team@users.noreply.github.com>
2025-05-25 00:40:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+//14645. 와이버스 부릉부릉
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    while(n--)
+    {
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
+    }
+    cout << "비와이";
+}
