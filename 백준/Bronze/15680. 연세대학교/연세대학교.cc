126679b78cc3315fd7f19098acd741231f881827
Xateam <57245851+xa-team@users.noreply.github.com>
2025-06-18 10:54:15 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,11 @@
+// 15680. 연세대학교
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << ((n == 0) ? "YONSEI" : "Leading the Way to the Future");
+}
