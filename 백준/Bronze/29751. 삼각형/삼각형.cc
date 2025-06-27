5fcf76292b3fb1ac0f0f787a7d55569a1638ad3a
Xateam <57245851+xa-team@users.noreply.github.com>
2025-06-27 16:41:20 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,13 @@
+// 29751. 삼각형
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float w, h;
+    cin >> w >> h;
+
+    float result = w * h / 2;
+    printf("%.01f", result);
+}
