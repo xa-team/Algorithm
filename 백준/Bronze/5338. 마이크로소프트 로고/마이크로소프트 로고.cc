7f6d2d07c33ec28c5f278e9263ebe2cd9d99689e
Xateam <57245851+xa-team@users.noreply.github.com>
2025-07-23 16:00:36 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,13 @@
+// 5338. 마이크로소프트 로고
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "       _.-;;-._" << std::endl;
+    std::cout << "'-..-'|   ||   |" << std::endl;
+    std::cout << "'-..-'|_.-;;-._|" << std::endl;
+    std::cout << "'-..-'|   ||   |" << std::endl;
+    std::cout << "'-..-'|_.-''-._|" << std::endl;
+    return 0;
+}
