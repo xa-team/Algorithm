fbd7606c1247cd6fed2d3233469ec5e1feb57b6d
Xateam <57245851+xa-team@users.noreply.github.com>
2025-08-19 23:19:47 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,18 @@
+// 5339. 콜센터
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "     /~\\\n";
+    cout << "    ( oo|\n";
+    cout << "    _\\=/_\n";
+    cout << "   /  _  \\\n";
+    cout << "  //|/.\\|\\\\\n";
+    cout << " ||  \\ /  ||\n";
+    cout << "============\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+}
