b021a9ca92652adf36900b65553c645ef048c476
Xateam <57245851+xa-team@users.noreply.github.com>
2025-08-25 23:20:40 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.cc
@@ -0,0 +1,26 @@
+// 20361. 일우는 야바위꾼
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, x, k;
+    cin >> n >> x >> k;
+
+    vector<bool> cups(n + 1, false);
+    cups[x] = true;
+    while (k-- > 0)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        bool tmp;
+        swap(cups[a], cups[b]);
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        if (cups[i] == true)
+            cout << i;
+    }
+}
