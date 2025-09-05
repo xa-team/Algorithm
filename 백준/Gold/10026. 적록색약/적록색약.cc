ebbabf94387f9702bc4931e6545815a9a44cb6a0
Xateam <57245851+xa-team@users.noreply.github.com>
2025-09-05 11:51:20 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -0,0 +1,151 @@
+// 10026. 적록색약
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+
+void DFS_B(int x, int y, int n, const vector<vector<char>> &graph, vector<vector<bool>> &visited)
+{
+    visited[y][x] = true;
+
+    for (int dir = 0; dir < 4; ++dir)
+    {
+        int nx = dx[dir] + x;
+        int ny = dy[dir] + y;
+
+        if (nx < n && nx >= 0 && ny < n && ny >= 0)
+        {
+            if (!visited[ny][nx] && graph[ny][nx] == 'B')
+                DFS_B(nx, ny, n, graph, visited);
+        }
+    }
+}
+void DFS_R(int x, int y, int n, const vector<vector<char>> &graph, vector<vector<bool>> &visited)
+{
+    visited[y][x] = true;
+
+    for (int dir = 0; dir < 4; ++dir)
+    {
+        int nx = dx[dir] + x;
+        int ny = dy[dir] + y;
+
+        if (nx < n && nx >= 0 && ny < n && ny >= 0)
+        {
+            if (!visited[ny][nx] && graph[ny][nx] == 'R')
+                DFS_R(nx, ny, n, graph, visited);
+        }
+    }
+}
+void DFS_G(int x, int y, int n, const vector<vector<char>> &graph, vector<vector<bool>> &visited)
+{
+    visited[y][x] = true;
+
+    for (int dir = 0; dir < 4; ++dir)
+    {
+        int nx = dx[dir] + x;
+        int ny = dy[dir] + y;
+
+        if (nx < n && nx >= 0 && ny < n && ny >= 0)
+        {
+            if (!visited[ny][nx] && graph[ny][nx] == 'G')
+                DFS_G(nx, ny, n, graph, visited);
+        }
+    }
+}
+void DFS_RG(int x, int y, int n, const vector<vector<char>> &graph, vector<vector<bool>> &visited)
+{
+    visited[y][x] = true;
+
+    for (int dir = 0; dir < 4; ++dir)
+    {
+        int nx = dx[dir] + x;
+        int ny = dy[dir] + y;
+
+        if (nx < n && nx >= 0 && ny < n && ny >= 0)
+        {
+            if (!visited[ny][nx] && (graph[ny][nx] == 'R' || graph[ny][nx] == 'G'))
+                DFS_RG(nx, ny, n, graph, visited);
+        }
+    }
+}
+
+int floodfill(int n, bool isColorweakness, const vector<vector<char>> &graph, vector<vector<bool>> &visited)
+{
+    int size = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (!visited[i][j] && graph[i][j] == 'B')
+            {
+                size++;
+                DFS_B(j, i, n, graph, visited);
+            }
+        }
+    }
+
+    if (isColorweakness)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (!visited[i][j] && (graph[i][j] == 'R' || graph[i][j] == 'G'))
+                {
+                    size++;
+                    DFS_RG(j, i, n, graph, visited);
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (!visited[i][j] && graph[i][j] == 'R')
+                {
+                    size++;
+                    DFS_R(j, i, n, graph, visited);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (!visited[i][j] && graph[i][j] == 'G')
+                {
+                    size++;
+                    DFS_G(j, i, n, graph, visited);
+                }
+            }
+        }
+    }
+
+    return size;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<char>> drawing(n, vector<char>(n));
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            cin >> drawing[i][j];
+
+    vector<vector<bool>> visitedColorweakness(n, vector<bool>(n, false));
+    vector<vector<bool>> visitedNormal(n, vector<bool>(n, false));
+
+    int areaSizeNormal = 0, areaSizeColorweakness = 0;
+    areaSizeColorweakness = floodfill(n, true, drawing, visitedColorweakness);
+    areaSizeNormal = floodfill(n, false, drawing, visitedNormal);
+
+    cout << areaSizeNormal << " " << areaSizeColorweakness;
+}
