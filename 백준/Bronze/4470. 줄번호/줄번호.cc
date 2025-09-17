33a18513a81c6eb3947375c35f82daf8137b8838
Xateam <57245851+xa-team@users.noreply.github.com>
2025-09-17 23:17:01 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,19 @@
+// 4470. 줄번호
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore();
+    for (int i = 1; i <= n; ++i)
+    {
+        string str;
+        getline(cin, str);
+
+        cout << i << ". " << str << "\n";
+    }
+}
