724fd146caca16ad408a00686a37c9b1a96df7ff
Xateam <57245851+xa-team@users.noreply.github.com>
2025-09-29 23:25:31 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,13 @@
+// 3003. 킹, 퀸, 룩, 비숏, 나이트, 폰
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    cout << 1 - a << ' ' << 1 - b << ' ' << 2 - c << ' ' << 2 - d << ' ' << 2 - e << ' ' << 8 - f;
+}
