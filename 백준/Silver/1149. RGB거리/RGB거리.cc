1acc75c1c3ef1e2c6590a617dc0c7fad44feedd0
Xateam <57245851+xa-team@users.noreply.github.com>
2025-12-22 20:04:53 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,5 +1,6 @@
 // 1149. RGB거리
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -10,30 +11,26 @@ int main()
     int n;
     cin >> n;
 
-    vector<vector<int>> street(n, vector<int>(3));
-    vector<vector<int>> DP(n, vector<int>(3, 0));
+    int R = 0, G = 0, B = 0;
+    int costR, costG, costB;
 
-    for (int i = 0; i < n; ++i)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        street[i][0] = a;
-        street[i][1] = b;
-        street[i][2] = c;
-    }
-
-    DP[0][0] = street[0][0];
-    DP[0][1] = street[0][1];
-    DP[0][2] = street[0][2];
+    cin >> costR >> costG >> costB;
+    R = costR;
+    G = costG;
+    B = costB;
 
     for (int i = 1; i < n; ++i)
     {
-        DP[i][0] = street[i][0] + min(DP[i - 1][1], DP[i - 1][2]);
-        DP[i][1] = street[i][1] + min(DP[i - 1][0], DP[i - 1][2]);
-        DP[i][2] = street[i][2] + min(DP[i - 1][1], DP[i - 1][0]);
-    }
-    int minRGB = __INT64_MAX__;
+        cin >> costR >> costG >> costB;
 
-    minRGB = min(min(DP[n - 1][0], DP[n - 1][1]), DP[n - 1][2]);
-    cout << minRGB;
+        int nextR = costR + min(G, B);
+        int nextG = costG + min(R, B);
+        int nextB = costB + min(R, G);
+
+        R = nextR;
+        G = nextG;
+        B = nextB;
+    }
+
+    cout << min({R, G, B});
 }
